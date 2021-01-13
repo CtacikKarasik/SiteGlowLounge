f720e5fee85f9d378eaf7c865ddbd59f4645a8d8
sperunov <stanislav.perunov@yandex.ru>
2021-01-13 15:40:19 +0300

    Create gulp-task for rename js files

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,37 @@
 var gulp = require('gulp');
 var rename = require('gulp-rename');
 
-gulp.task('default', function() {
+gulp.task('main_js', function() {
+    return gulp .src('src/original_source/js/*.js')
+                .pipe(rename({ suffix: ".min" }))
+                .pipe(gulp.dest('src/js'));
+});
+
+gulp.task('services_js', function() {
+    return gulp .src('src/original_source/js/services/*.js')
+                .pipe(rename({ suffix: ".min" }))
+                .pipe(gulp.dest('src/js/services'));
+});
+
+gulp.task('vm_js', function() {
+    return gulp .src('src/original_source/js/vm/*.js')
+                .pipe(rename({ suffix: ".min" }))
+                .pipe(gulp.dest('src/js/vm'));
+});
+
+gulp.task('lib_js', function() {
+    return gulp .src('src/original_source/js/lib/*.js')
+                .pipe(rename({ suffix: ".min" }))
+                .pipe(gulp.dest('src/js/lib'));
+});
+
+gulp.task('css', function() {
     return gulp .src('src/original_source/css/*.css')
                 .pipe(rename({ suffix: ".min" }))
                 .pipe(gulp.dest('src/css'));
 });
 
+gulp.task('js', gulp.parallel('main_js', 'services_js', 'vm_js', 'lib_js'));
 
+gulp.task('default', gulp.series('js', 'css'));
   
